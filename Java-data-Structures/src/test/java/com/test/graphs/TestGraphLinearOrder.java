bd9f4a3ad0db767255a4a9826cca5b446769d867
KAKANAKOU Miguel Stephane <because_of_peace@yahoo.com>
2017-05-16 13:03:05 +0800

    Add JUnit test for GraphLinearOrder

--- Java-data-Structures/src/test/java/com/test/graphs/TestGraphLinearOrder.java
+++ Java-data-Structures/src/test/java/com/test/graphs/TestGraphLinearOrder.java
@@ -1,15 +1,18 @@
 package com.test.graphs;
 
-import com.graphs.DetectCycle;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.graphs.GraphAdjacencyListRepresentation;
+import com.graphs.GraphLinearOrder;
 
 import junit.framework.TestCase;
 
-public class TestDetectCycle extends TestCase {
+public class TestGraphLinearOrder extends TestCase{
 
-    public void testDetectCycleInDirectedGraph() {
+    public void testGraphLinearOrder() {
         GraphAdjacencyListRepresentation graph = new GraphAdjacencyListRepresentation(13, true);
-
+        
         graph.addEdge(0, 5);
         graph.addEdge(0, 1);
 
@@ -44,8 +47,8 @@ public class TestDetectCycle extends TestCase {
 
         graph.addEdge(12, 9);
         
-        assertEquals(true, DetectCycle.haveCycle(graph));
-        
+        List<Integer> list = GraphLinearOrder.linearOrder(graph);
+        assertEquals(null, list);
         
         graph = new GraphAdjacencyListRepresentation(13, true);
 
@@ -72,8 +75,28 @@ public class TestDetectCycle extends TestCase {
         graph.addEdge(9, 12);
 
         graph.addEdge(11, 12);
-
-        assertEquals(false, DetectCycle.haveCycle(graph));
+        
+        List<Integer> expectedList = new ArrayList<>();
+        expectedList.add(8);
+        expectedList.add(7);
+        expectedList.add(2);
+        expectedList.add(3);
+        expectedList.add(0);
+        expectedList.add(6);
+        expectedList.add(9);
+        expectedList.add(10);
+        expectedList.add(11);
+        expectedList.add(12);
+        expectedList.add(1);
+        expectedList.add(5);
+        expectedList.add(4);
+        
+        list = GraphLinearOrder.linearOrder(graph);
+        
+        System.out.println(list);
+        
+        assertEquals(true, list.equals(expectedList));
+        
         
     }
     
