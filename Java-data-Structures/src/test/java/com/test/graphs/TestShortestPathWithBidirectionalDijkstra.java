43553ded65db9e292c2741c4b097b0e963167fab
KAKANAKOU Miguel Stephane <because_of_peace@yahoo.com>
2017-05-18 13:43:52 +0800

    Add JUnit for ShortestPathWithBidirectionalDijkstra

---
+++ Java-data-Structures/src/test/java/com/test/graphs/TestShortestPathWithBidirectionalDijkstra.java
@@ -0,0 +1,60 @@
+package com.test.graphs;
+
+import java.util.LinkedList;
+
+import com.graphs.GraphAdjacencyListRepresentation;
+import com.graphs.ShortestPathWithBidirectionalDijkstra;
+
+import junit.framework.TestCase;
+
+public class TestShortestPathWithBidirectionalDijkstra extends TestCase{
+    
+    public void testShortestPathWithBidirectionalDijkstra() {
+        
+        GraphAdjacencyListRepresentation graph = new GraphAdjacencyListRepresentation(13, true);
+        graph.addEdge(0, 5, 3);
+        graph.addEdge(0, 1, 9);
+
+        graph.addEdge(2, 0, 2);
+        graph.addEdge(2, 3, 15);
+
+        graph.addEdge(3, 5, 14);
+        graph.addEdge(3, 2, 8);
+
+        graph.addEdge(4, 3, 44);
+        graph.addEdge(4, 2, 6);
+
+        graph.addEdge(5, 4, 1);
+
+        graph.addEdge(6, 4, 2);
+        graph.addEdge(6, 8, 3);
+        graph.addEdge(6, 0, 11);
+        graph.addEdge(6, 9, 23);
+
+        graph.addEdge(7, 6, 9);
+
+        graph.addEdge(8, 6, 3);
+
+        graph.addEdge(9, 11, 8);
+        graph.addEdge(9, 10, 5);
+
+        graph.addEdge(10, 12, 17);
+
+        graph.addEdge(11, 12, 33);
+
+        graph.addEdge(12, 9, 10);
+        
+        ShortestPathWithBidirectionalDijkstra sp = new ShortestPathWithBidirectionalDijkstra(graph, 6, 3);
+        assertEquals(23, sp.getShortestDistance());
+        
+        LinkedList<Integer> pathTo3 = new LinkedList<>();
+        pathTo3.add(6);
+        pathTo3.add(4);
+        pathTo3.add(2);
+        pathTo3.add(3);
+        
+        assertEquals(true, pathTo3.equals(sp.getShortestPath()));
+        
+    }
+
+}
